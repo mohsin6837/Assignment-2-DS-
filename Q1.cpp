5e66a7b55dd2ae2a38ad481dbefed8cbbf61eb91
mohsin6837 <68821359+mohsin6837@users.noreply.github.com>
2021-10-09 18:20:56 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data; 
+    node* next; 
+    node* prev; 
+}*head=NULL,*tail; 
+void create(){
+    int n; 
+    cout<<"Enter the no of nodes:";
+    cin>>n; 
+    node* temp; 
+    for(int i=0;i<n;i++){
+        temp=new node; 
+        cout<<"Enter data : "<<i+1; 
+        cin>>temp->data; 
+        temp->next=NULL;
+        temp->prev=NULL;
+        if(head==NULL) {
+            head=temp;
+            tail=temp;
+        }
+        else{
+            tail->next=temp;
+            temp->prev=tail;
+            tail=temp;
+    }
+}
+}
+void view(){
+    node* traverse; 
+    traverse=head;
+    while(traverse!=NULL){
+        cout<<traverse->data<<" "; 
+        traverse=traverse->next;  
+    }
+    cout<<endl;
+}
+void even(){
+    node* c=head;
+    cout<<"Even numbers in the doubly linked linked list";
+    while(c!=NULL){
+        if(c->data%2==0){
+           cout<<c->data<<" ";
+        }
+        c=c->next;
+    }
+    
+}
+int main(){
+  create();
+  cout<<"Doubly linked list is: ";
+  view();
+  even();
+    return 0;
+}
